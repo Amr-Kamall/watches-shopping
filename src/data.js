fffed1f822ba83a56aab370e614e1d7b1f2d9975
amr <amr442028@gmail.com>
2024-01-31 01:29:06 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,104 @@
+let featuredData = [
+  {
+    id: "jfhgbvnscs",
+    name: "JAZZMASTER",
+    price: "$1050",
+    img: "images/featured1.png",
+  },
+  {
+    id: "denijdxjm",
+    name: "INGERSOLL",
+    price: "$250",
+    img: "images/featured2.png",
+  },
+  {
+    id: "dkesklamdlm",
+    name: "JAZZMASTER",
+    price: "$1050",
+    img: "images/featured3.png",
+  },
+];
+
+let productsData = [
+  {
+    id: "dejmldxmk",
+    name: "SPIRIT ROSE",
+    price: "$1500",
+    img: "images/new1.png",
+  },
+  {
+    id: "eijdjde",
+    name: "KHAKI PILOT",
+    price: "$1350",
+    img: "images/product2.png",
+  },
+  {
+    id: "ednkendmka",
+    name: "JUBILEE BLACK",
+    price: "$870",
+    img: "images/product3.png",
+  },
+  {
+    id: "edhdeah",
+    name: "FOSIL ME3",
+    price: "$650",
+    img: "images/product4.png",
+  },
+  {
+    id: "adnjnadkj",
+    name: "DUCHEN",
+    price: "$950",
+    img: "images/product5.png",
+  },
+];
+
+let newsData = [
+  {
+    id: "djdhxbd",
+    name: "LONGINES ROSE",
+    price: "$980",
+    img: "images/new1.png",
+  },
+  {
+    id: "edmakldmla",
+    name: "JAZZMASTER",
+    price: "$1150",
+    img: "images/new2.png",
+  },
+  {
+    id: "eddmeled",
+    name: "DREYFUSS GOLD",
+    price: "$750",
+    img: "images/new3.png",
+  },
+  {
+    id: "admklmea",
+    name: "PORTUGUESE ROSE",
+    price: "$1590",
+    img: "images/new4.png",
+  },
+];
+
+let testimonialData = [
+  {
+    opinion:
+      "They are the best watches that one acquires, also they are always with the latest news and trends, with a very comfortable price and especially with the attention you receive, they are always attentive to your questions.",
+    name: "Lee Doe",
+    data: "March 27. 2021",
+    img: "images/testimonial1.png",
+  },
+  {
+    opinion:
+      "They are the best watches that one acquires, also they are always with the latest news and trends, with a very comfortable price and especially with the attention you receive, they are always attentive to your questions.",
+    name: "Samantha Mey",
+    data: "April 19. 2021",
+    img: "images/testimonial3.png",
+  },
+  {
+    opinion:
+      "They are the best watches that one acquires, also they are always with the latest news and trends, with a very comfortable price and especially with the attention you receive, they are always attentive to your questions.",
+    name: "Lee Doe",
+    data: "Jul 10. 2021",
+    img: "images/testimonial3.png",
+  },
+];
---
+++ src/main.js
@@ -0,0 +1,104 @@
+let close = document.querySelector(".close-icon");
+let bar = document.querySelector(".bar-icon");
+let navbar = document.querySelector(".navbar-list");
+let links = document.querySelectorAll(".navbar-list .nav-link");
+
+bar.addEventListener("click", function () {
+  navbar.style.top = "0%";
+});
+
+close.addEventListener("click", function () {
+  navbar.style.top = "-100%";
+});
+
+links.forEach((link) =>
+  link.addEventListener("click", function () {
+    links.forEach((link) => link.classList.remove("active"));
+    link.classList.add("active");
+  })
+);
+
+function handleClicked(e) {
+  if (navbar && navbar.contains(e.target)) {
+    navbar.style.top = "-100%";
+  } else {
+    console.log("we clicked outside");
+  }
+}
+window.addEventListener("click", handleClicked, true);
+
+// featured data
+let featuredContainer = document.querySelector(".featured-container");
+featuredContainer.innerHTML = featuredData
+  .map((ele) => {
+    const { img, name, price } = ele;
+    return `
+    <div class="watching-box">
+        <span class="sale">sale</span>
+        <img src=${img} />
+        <h3 class="watching-name">${name}</h3>
+        <p class="watching-price">${price}</p>
+        <button class="btn-add">add to cart</button>
+    </div>
+  `;
+  })
+  .join("");
+// end featured data
+
+//product-data
+let productedContainer = document.querySelector(".product-container");
+
+productedContainer.innerHTML = productsData
+  .map((ele) => {
+    const { img, name, price } = ele;
+    return `
+    <div class="watching-box">
+        <span class="bag"><i class="bi bi-bag"></i></span>
+        <img src=${img} />
+        <h3 class="watching-name">${name}</h3>
+        <p class="watching-price">${price}</p>
+    </div>
+  `;
+  })
+  .join("");
+//end product data
+
+//new data
+
+let newContainer = document.querySelector(".new-container");
+newContainer.innerHTML = newsData
+  .map((ele) => {
+    const { img, name, price } = ele;
+    return `
+    <div class="watching-box">
+        <span class="new-icon">New</span>
+        <img src=${img} />
+        <h3 class="watching-name">${name}</h3>
+        <p class="watching-price">${price}</p>
+        <button class="btn-add">add to cart</button>
+    </div>
+  `;
+  })
+  .join("");
+
+//end new data
+
+// slider
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".right-arrow",
+    prevEl: ".left-arrow",
+  },
+});
+// Custom button click handlers
+document.querySelector(".left-arrow").addEventListener("click", function () {
+  swiper.slidePrev();
+});
+
+document.querySelector(".right-arrow").addEventListener("click", function () {
+  swiper.slideNext();
+});
+// end slider
