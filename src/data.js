f4c7fcc896076e36d6125045e62294a7a1ac0964
amr <amr442028@gmail.com>
2024-02-01 20:24:10 +0200

    first commit

--- src/data.js
+++ src/data.js
@@ -2,19 +2,19 @@ let featuredData = [
   {
     id: "jfhgbvnscs",
     name: "JAZZMASTER",
-    price: "$1050",
+    price: "1050",
     img: "images/featured1.png",
   },
   {
     id: "denijdxjm",
     name: "INGERSOLL",
-    price: "$250",
+    price: "250",
     img: "images/featured2.png",
   },
   {
     id: "dkesklamdlm",
-    name: "JAZZMASTER",
-    price: "$1050",
+    name: "ROSE GOLD",
+    price: "1050",
     img: "images/featured3.png",
   },
 ];
@@ -23,31 +23,31 @@ let productsData = [
   {
     id: "dejmldxmk",
     name: "SPIRIT ROSE",
-    price: "$1500",
+    price: "1500",
     img: "images/new1.png",
   },
   {
     id: "eijdjde",
     name: "KHAKI PILOT",
-    price: "$1350",
+    price: "1350",
     img: "images/product2.png",
   },
   {
     id: "ednkendmka",
     name: "JUBILEE BLACK",
-    price: "$870",
+    price: "870",
     img: "images/product3.png",
   },
   {
     id: "edhdeah",
     name: "FOSIL ME3",
-    price: "$650",
+    price: "650",
     img: "images/product4.png",
   },
   {
     id: "adnjnadkj",
     name: "DUCHEN",
-    price: "$950",
+    price: "950",
     img: "images/product5.png",
   },
 ];
@@ -56,49 +56,100 @@ let newsData = [
   {
     id: "djdhxbd",
     name: "LONGINES ROSE",
-    price: "$980",
+    price: "980",
     img: "images/new1.png",
   },
   {
     id: "edmakldmla",
-    name: "JAZZMASTER",
-    price: "$1150",
+    name: "Egyptian Rose",
+    price: "1150",
     img: "images/new2.png",
   },
   {
     id: "eddmeled",
     name: "DREYFUSS GOLD",
-    price: "$750",
+    price: "750",
     img: "images/new3.png",
   },
   {
     id: "admklmea",
     name: "PORTUGUESE ROSE",
-    price: "$1590",
+    price: "1590",
     img: "images/new4.png",
   },
 ];
 
-let testimonialData = [
+let allData = [
   {
-    opinion:
-      "They are the best watches that one acquires, also they are always with the latest news and trends, with a very comfortable price and especially with the attention you receive, they are always attentive to your questions.",
-    name: "Lee Doe",
-    data: "March 27. 2021",
-    img: "images/testimonial1.png",
+    id: "jfhgbvnscs",
+    name: "JAZZMASTER",
+    price: "1050",
+    img: "images/featured1.png",
   },
   {
-    opinion:
-      "They are the best watches that one acquires, also they are always with the latest news and trends, with a very comfortable price and especially with the attention you receive, they are always attentive to your questions.",
-    name: "Samantha Mey",
-    data: "April 19. 2021",
-    img: "images/testimonial3.png",
+    id: "denijdxjm",
+    name: "INGERSOLL",
+    price: "250",
+    img: "images/featured2.png",
   },
   {
-    opinion:
-      "They are the best watches that one acquires, also they are always with the latest news and trends, with a very comfortable price and especially with the attention you receive, they are always attentive to your questions.",
-    name: "Lee Doe",
-    data: "Jul 10. 2021",
-    img: "images/testimonial3.png",
+    id: "dkesklamdlm",
+    name: "ROSE GOLD",
+    price: "1050",
+    img: "images/featured3.png",
+  },
+  {
+    id: "dejmldxmk",
+    name: "SPIRIT ROSE",
+    price: "1500",
+    img: "images/new1.png",
+  },
+  {
+    id: "eijdjde",
+    name: "KHAKI PILOT",
+    price: "1350",
+    img: "images/product2.png",
+  },
+  {
+    id: "ednkendmka",
+    name: "JUBILEE BLACK",
+    price: "870",
+    img: "images/product3.png",
+  },
+  {
+    id: "edhdeah",
+    name: "FOSIL ME3",
+    price: "650",
+    img: "images/product4.png",
+  },
+  {
+    id: "adnjnadkj",
+    name: "DUCHEN",
+    price: "950",
+    img: "images/product5.png",
+  },
+  {
+    id: "djdhxbd",
+    name: "LONGINES ROSE",
+    price: "980",
+    img: "images/new1.png",
+  },
+  {
+    id: "edmakldmla",
+    name: "Egyptian Rose",
+    price: "1150",
+    img: "images/new2.png",
+  },
+  {
+    id: "eddmeled",
+    name: "DREYFUSS GOLD",
+    price: "750",
+    img: "images/new3.png",
+  },
+  {
+    id: "admklmea",
+    name: "PORTUGUESE ROSE",
+    price: "1590",
+    img: "images/new4.png",
   },
 ];
--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ let close = document.querySelector(".close-icon");
 let bar = document.querySelector(".bar-icon");
 let navbar = document.querySelector(".navbar-list");
 let links = document.querySelectorAll(".navbar-list .nav-link");
+let shoppingContainer = document.querySelector(".shopping-container");
+let shoppingCartIcon = document.querySelector(".shopping-cart-icon");
+let shoppingCart = document.querySelector(".shopping-cart");
+let closeShoppingCart = document.querySelector(".close-icon-cart");
+let label = document.querySelector(".label");
+let shoppingTitle = document.querySelector(".shopping-title");
+let moreDetails = document.querySelector(".more-details");
+let basket = JSON.parse(localStorage.getItem("basket")) || [];
+let numItems = document.querySelector(".num-items");
+
+shoppingCartIcon.addEventListener("click", function () {
+  shoppingCart.style.left = "0%";
+});
+closeShoppingCart.addEventListener("click", function () {
+  shoppingCart.style.left = "100%";
+});
 
 bar.addEventListener("click", function () {
   navbar.style.top = "0%";
@@ -31,18 +47,19 @@ window.addEventListener("click", handleClicked, true);
 let featuredContainer = document.querySelector(".featured-container");
 featuredContainer.innerHTML = featuredData
   .map((ele) => {
-    const { img, name, price } = ele;
+    let { img, name, price, id } = ele;
     return `
-    <div class="watching-box">
+    <div class="watching-box" id=${id}>
         <span class="sale">sale</span>
         <img src=${img} />
         <h3 class="watching-name">${name}</h3>
         <p class="watching-price">${price}</p>
-        <button class="btn-add">add to cart</button>
+        <button class="btn-add" onclick="addToCart(${id})" >add to cart</button>
     </div>
   `;
   })
   .join("");
+
 // end featured data
 
 //product-data
@@ -50,10 +67,10 @@ let productedContainer = document.querySelector(".product-container");
 
 productedContainer.innerHTML = productsData
   .map((ele) => {
-    const { img, name, price } = ele;
+    const { img, name, price, id } = ele;
     return `
-    <div class="watching-box">
-        <span class="bag"><i class="bi bi-bag"></i></span>
+    <div class="watching-box" id=${id}>
+        <span class="bag"><i class="bi bi-bag" onclick="addToCart(${id})"></i></span>
         <img src=${img} />
         <h3 class="watching-name">${name}</h3>
         <p class="watching-price">${price}</p>
@@ -68,14 +85,14 @@ productedContainer.innerHTML = productsData
 let newContainer = document.querySelector(".new-container");
 newContainer.innerHTML = newsData
   .map((ele) => {
-    const { img, name, price } = ele;
+    const { img, name, price, id } = ele;
     return `
-    <div class="watching-box">
+    <div class="watching-box" id=${id} >
         <span class="new-icon">New</span>
         <img src=${img} />
         <h3 class="watching-name">${name}</h3>
         <p class="watching-price">${price}</p>
-        <button class="btn-add">add to cart</button>
+        <button class="btn-add" onclick="addToCart(${id})" >add to cart</button>
     </div>
   `;
   })
@@ -102,3 +119,134 @@ document.querySelector(".right-arrow").addEventListener("click", function () {
   swiper.slideNext();
 });
 // end slider
+
+function addToCart(id) {
+  const selectedItem = id;
+  const search = basket.find((x) => x.id === selectedItem.id);
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    return;
+  }
+  localStorage.setItem("basket", JSON.stringify(basket));
+  // console.log(basket);
+  generateWatchingCarts();
+}
+
+//content of my cart 😍⚡
+
+function generateWatchingCarts() {
+  if (basket.length !== 0) {
+    label.innerHTML = "";
+    moreDetails.innerHTML = `
+        <p class="num-items">${calcItems()} <span>items</span></p>
+        <p class="total-price">$${totalPrice()}</p>
+        `;
+    shoppingTitle.innerHTML = `<h1 class="shopping-title">My Cart</h1>`;
+    shoppingContainer.innerHTML = basket
+      .map((ele) => {
+        let { id, item } = ele;
+        const search = allData.find((x) => x.id === id) || [];
+        // console.log(search);
+        let { img, name, price } = search;
+        return `
+            <div class="cart-box" class=${id} >
+              <div class="cart-img-content">
+                <img src=${img} alt=${name} />
+              </div>
+              <div class="cart-data">
+                <p class="cart-name">${name}</p>
+                <p class="cart-price">$${price * item}</p> 
+                <div class="cart-buttons">
+                  <div class="plus-minus-btns"> 
+                  <i class="bi bi-dash-lg" onclick="decreaseItem(${id})" ></i>
+                  <span class=${id}>${item ? item : 0}</span>
+                  <i class="bi bi-plus-lg" onclick="increaseItem(${id})" ></i>
+                  </div>              
+                    <i class="fa-regular fa-trash-can" onclick="removeItem(${id})" ></i>
+                </div>
+              </div>
+            </div>
+        `;
+      })
+      .join("");
+  } else {
+    label.innerHTML = `<h1>empty cart</h1>`;
+    shoppingTitle.innerHTML = "";
+    moreDetails.innerHTML = "";
+  }
+  // localStorage.setItem("basket", JSON.stringify(basket));
+}
+
+generateWatchingCarts();
+
+function increaseItem(id) {
+  const selectedItem = id;
+  const search = basket.find((x) => x.id === selectedItem.id);
+  if (search) {
+    search.item += 1; //update item of the basket
+  } else {
+    return;
+  }
+  localStorage.setItem("basket", JSON.stringify(basket));
+  updateItem(id);
+  generateWatchingCarts();
+  // calcItems();
+}
+
+function decreaseItem(id) {
+  const selectedItem = id;
+  const search = basket.find((x) => x.id === selectedItem.id);
+  const filteredIndex = basket.findIndex((x) => x.id === selectedItem.id);
+
+  if (search.item > 1) {
+    search.item -= 1;
+  } else {
+    basket.splice(filteredIndex, 1);
+  }
+
+  localStorage.setItem("basket", JSON.stringify(basket));
+  updateItem(selectedItem);
+  generateWatchingCarts();
+  // calcItems();
+}
+
+function updateItem(selectedItem) {
+  const search = basket.find((x) => x.id === selectedItem.id);
+  if (search) {
+    document.querySelector(`.${search.id}`).innerHTML = search.item;
+  } else {
+    return;
+  }
+}
+
+function removeItem(id) {
+  const selectedItem = id;
+  const searchIndex = basket.findIndex((x) => x.id === selectedItem.id);
+  // console.log(searchIndex);
+  // basket = basket.filter((ele) => ele.id !== selectedItem.id);
+  basket.splice(searchIndex, 1);
+  generateWatchingCarts();
+  localStorage.setItem("basket", JSON.stringify(basket));
+}
+
+function calcItems() {
+  let calcItems = basket
+    .map((ele) => ele.item)
+    .reduce((acc, cur) => acc + cur, 0);
+  return calcItems;
+}
+
+function totalPrice() {
+  const total = basket
+    .map((ele) => {
+      let { item, id } = ele;
+      const search = allData.find((x) => x.id === id);
+      return search.price * item;
+    })
+    .reduce((x, y) => x + y);
+  return total;
+}
